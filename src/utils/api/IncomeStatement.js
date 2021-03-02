1f2ccd57ea6625dd60b40a2a695f0568a9da727b
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-03-02 19:39:02 +0000

    added new names for all fetches, updates on balance sheet in attempt to fix bug, unsuccessful

--- src/utils/api/IncomeStatement.js
+++ src/utils/api/IncomeStatement.js
@@ -23,6 +23,7 @@ const IncomeStatement = (text, setText) => {
         fetch(`https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=${text.text}&apikey=ARU6VBZ6KLPWOGUD`)
       .then((res) => res.json())
       .then((data) => { 
+        console.log(data)
         let arr = incStat
           arr.push(data)
           setIncStat(arr)
@@ -40,27 +41,32 @@ const IncomeStatement = (text, setText) => {
                   <div key={index}>
                   <h1>Income Statement</h1>
                   <p>Fiscal Date Ending: {data.annualReports[0].fiscalDateEnding}</p>
-                  <p>Total Revenue: ${data.annualReports[0].totalRevenue}</p>
-                  <p>Total Operating Expense: ${data.annualReports[0].totalOperatingExpense}</p>
-                  <p>Cost of Revenue: ${data.annualReports[0].costOfRevenue}</p>
-                  <p>Gross Profit: ${data.annualReports[0].grossProfit}</p>
-                  <p>Earnings Before Interest and Taxes: ${data.annualReports[0].ebit}</p>
-                  <p>Net Income: ${data.annualReports[0].netIncome}</p>
-                  <p>Research and Development: ${data.annualReports[0].researchAndDevelopment}</p>
-                  <p>Income Before Tax: ${data.annualReports[0].incomeBeforeTax}</p>
-                  <p>Minority Interest: ${data.annualReports[0].minorityInterest}</p>
-                  <p>Selling General Administrative: ${data.annualReports[0].sellingGeneralAdministrative}</p>
-                  <p>Operating Income: ${data.annualReports[0].operatingIncome}</p>
-                  <p>Other Operating Expense: ${data.annualReports[0].otherOperatingExpense}</p>
-                  <p>Interest Expense: ${data.annualReports[0].interestExpense}</p>
-                  <p>Tax Provision: ${data.annualReports[0].taxProvision}</p>
-                  <p>Net Interest Income: ${data.annualReports[0].netInterestIncome}</p>
-                  <p>Extraordinary Items: ${data.annualReports[0].extraordinaryItems}</p>
-                  <p>Income Tax Expense: ${data.annualReports[0].incomeTaxExpense}</p>
-                  <p>Total Other Income Expense: ${data.annualReports[0].totalOtherIncomeExpense}</p>
-                  <p>Discontinued Operations: ${data.annualReports[0].discontinuedOperations}</p>
-                  <p>Net Income From Continuing Operations: ${data.annualReports[0].netIncomeFromContinuingOperations}</p>
-                  <p>Net Income Applicable to Common Shares: ${data.annualReports[0].netIncomeApplicableToCommonShares}</p>
+                  <p>Reported Currency: {data.annualReports[0].reportedCurrency}</p>
+                  <br/>
+                  <p>Comprehensive Income Net Of Tax: {data.annualReports[0].comprehensiveIncomeNetOfTax}</p>
+                  <p>Cost Of Revenue: {data.annualReports[0].costOfRevenue}</p>
+                  <p>Cost of Goods And Services Sold: {data.annualReports[0].costofGoodsAndServicesSold}</p>
+                  <p>Depreciation: {data.annualReports[0].depreciation}</p>
+                  <p>Depreciation And Amortization: {data.annualReports[0].depreciationAndAmortization}</p>
+                  <p>Ebit: {data.annualReports[0].ebit}</p>
+                  <p>Ebitda: {data.annualReports[0].ebitda}</p>
+                  <p>Gross Profit: {data.annualReports[0].grossProfit}</p>
+                  <p>Income Before Tax: {data.annualReports[0].incomeBeforeTax}</p>
+                  <p>Income Tax Expense: {data.annualReports[0].incomeTaxExpense}</p>
+                  <p>Interest And Debt Expense: {data.annualReports[0].interestAndDebtExpense}</p>
+                  <p>Interest Expense: {data.annualReports[0].interestExpense}</p>
+                  <p>Interest Income: {data.annualReports[0].interestIncome}</p>
+                  <p>Investment IncomeNet: {data.annualReports[0].investmentIncomeNet}</p>
+                  <p>Net Income: {data.annualReports[0].netIncome}</p>
+                  <p>Net Income From Continuing Operations: {data.annualReports[0].netIncomeFromContinuingOperations}</p>
+                  <p>Net Interest Income: {data.annualReports[0].netInterestIncome}</p>
+                  <p>Non Interest Income: {data.annualReports[0].nonInterestIncome}</p>
+                  <p>Operating Expenses: {data.annualReports[0].operatingExpenses}</p>
+                  <p>Operating Income: {data.annualReports[0].operatingIncome}</p>
+                  <p>Other Non OperatingIncome: {data.annualReports[0].otherNonOperatingIncome}</p>
+                  <p>Researc And Development: {data.annualReports[0].researchAndDevelopment}</p>
+                  <p>Selling General And Administrative: {data.annualReports[0].sellingGeneralAndAdministrative}</p>
+                  <p>Total Revenue: {data.annualReports[0].totalRevenue}</p>
                 </div>
                 )
               }))}
