ce390f7a39944ec9001334632a2df832a5db1fef
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-03-03 16:33:37 +0000

    updated  issues on market stack

--- src/utils/api/FunctionForm.js
+++ src/utils/api/FunctionForm.js
@@ -25,7 +25,7 @@ const Intraday = (text, setText) => {
         {
          options: {
              chart: {
-                 background: '#f4f4f4',
+                 background: '#02030d',
                  foreColor: '#333',
               },
          style: {
