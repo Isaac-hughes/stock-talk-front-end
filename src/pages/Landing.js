b4512a7be98bbbddc7caf7187fdbc1b2fec6b6d6
Colby Robinson <75703870+colbyrob1@users.noreply.github.com>
2021-03-03 11:57:55 +0000

    updated changes

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -14,12 +14,7 @@ const Landing = ({user, setUser, isAuthenticated, setIsAuthenticated}) => {
 
   return (
     <div className="Landing">
-<<<<<<< HEAD
-      <h1>Sociabull</h1>
-      <nav>
-=======
       <nav className="mainNav">
->>>>>>> 8ab0875ce008e4bbdc30af20a5a683b4daa74bd7
         <button>
           <Link to="/home">Home</Link>
         </button>
@@ -30,14 +25,10 @@ const Landing = ({user, setUser, isAuthenticated, setIsAuthenticated}) => {
           <Link to="/signup">Sign Up</Link>
         </button>
       </nav>
-<<<<<<< HEAD
-      
-=======
       <h1>Sociabull</h1>
         <video width="400px" height="400px" autoPlay loop muted className="top-vid">
           <source src={clip} type="video/mp4" />
         </video>
->>>>>>> 8ab0875ce008e4bbdc30af20a5a683b4daa74bd7
     </div>
   );
 }
